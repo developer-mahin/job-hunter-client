753f26d1075134c0d4a0327c3840aaaa8b5dd33a
developer-mahin <mdmahin1310@gmail.com>
2024-12-21 22:48:07 +0600

    change base url

--- src/constant/baseurl.ts
+++ src/constant/baseurl.ts
@@ -1,2 +1,2 @@
-// export const baseurl = `https://jobhunterserver.vercel.app/api/v1`;
-export const baseurl = `http://localhost:5000/api/v1`;
+export const baseurl = `https://jobhunterserver.vercel.app/api/v1`;
+// export const baseurl = `http://localhost:5000/api/v1`;
